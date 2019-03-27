936bd5a001a964b0d8fd776ccc016b3932831446
Long Hung Nguyen <longhungn98@gmail.com>
2019-03-27 20:53:48 +1100

    config for heroku postgres

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,12 +1,24 @@
 require('dotenv').config();
 
-module.exports = {
-  type: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  entities: [__dirname + '/**/*.{entity,repository}{.ts,.js}'],
-  synchronize: true,
-};
+let config;
+if (process.env.DATABASE_URL) {
+  config = {
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: [__dirname + '/**/*.{entity,repository}{.ts,.js}'],
+    synchronize: true,
+  };
+} else {
+  config = {
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    entities: [__dirname + '/**/*.{entity,repository}{.ts,.js}'],
+    synchronize: true,
+  };
+}
+
+module.exports = config;
